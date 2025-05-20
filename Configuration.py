1d17badcb9b28e5035fbd6f3e0fea7b6e864934a
lucylucerostar-1994 <Lucylucerostar.1994@gmail.com>
2025-05-19 21:25:43 -0700

    Create Configuration.py

---
+++ Configuration.py
@@ -0,0 +1,9 @@
+
+
+URL_SERVICE = "https://cnt-75b1364e-7898-4742-b4d8-44fa71ca6c7c.containerhub.tripleten-services.com"
+
+API_DOCS = "/docs/"
+
+CREATE_USER_PATH = "/api/v1/users"
+
+KITS_PATH = "/api/v1/kits"
