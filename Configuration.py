06155a455aa0c8594a815256bbc0f7685f64f2a6
lucylucerostar-1994 <Lucylucerostar.1994@gmail.com>
2025-05-24 00:00:50 -0700

    -m "Remove unnecessary files"

--- Configuration.py
+++
@@ -1,9 +0,0 @@
-
-
-URL_SERVICE = "https://cnt-75b1364e-7898-4742-b4d8-44fa71ca6c7c.containerhub.tripleten-services.com"
-
-API_DOCS = "/docs/"
-
-CREATE_USER_PATH = "/api/v1/users"
-
-KITS_PATH = "/api/v1/kits"
