d6c7480da33e371e1d80f017c3b3862112dd5ec0
lucylucerostar-1994 <Lucylucerostar.1994@gmail.com>
2025-06-01 18:28:18 -0700

    helpers.py

--- helpers.py
+++ helpers.py
@@ -1,54 +1,54 @@
-import logging
-from selenium import webdriver
-from pages import UrbanRoutesPage
-from data import URBAN_ROUTES_URL, ADDRESS_FROM, ADDRESS_TO, PHONE_NUMBER, CARD_NUMBER, CARD_EXPIRY_DATE, CARD_CVV, MESSAGE_FOR_DRIVER
+# Retrieves Phone code. Do not change
+# File should be completely unchanged
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
+def retrieve_phone_code(driver) -> str:
+    """This code retrieves phone confirmation number and returns it as a string.
+    Use it when application waits for the confirmation code to pass it into your tests.
+    The phone confirmation code can only be obtained after it was requested in application."""
 
-class TestUrbanRoutes:
+    import json
+    import time
 
-    @classmethod
-    def setup_class(cls):
-        from selenium.webdriver.chrome.options import Options
+    from selenium.common import WebDriverException
+    code = None
+    for i in range(10):
+        try:
+            logs = [log["message"] for log in driver.get_log('performance') if log.get("message")
+                    and 'api/v1/number?number' in log.get("message")]
+            for log in reversed(logs):
+                message_data = json.loads(log)["message"]
+                body = driver.execute_cdp_cmd('Network.getResponseBody',
+                                              {'requestId': message_data["params"]["requestId"]})
+                code = ''.join([x for x in body['body'] if x.isdigit()])
+        except WebDriverException:
+            time.sleep(1)
+            continue
+        if not code:
+            raise Exception("No phone confirmation code found.\n"
+                            "Please use retrieve_phone_code only after the code was requested in your application.")
+        return code
 
-        # Set Chrome options
-        chrome_options = Options()
-        chrome_options.set_capability("goog:loggingPrefs", {'performance': 'ALL'})
-        cls.driver = webdriver.Chrome(options=chrome_options)
 
-    def test_set_route(self):
-        self.driver.get(URBAN_ROUTES_URL)  # Open the URL for this test
-        routes_page = UrbanRoutesPage(self.driver)
-        routes_page.set_address(ADDRESS_FROM, ADDRESS_TO)  # Set 'from' and 'to' addresses
-        assert routes_page.get_from() == ADDRESS_FROM, f"Expected {ADDRESS_FROM}, but got {routes_page.get_from()}"
-        assert routes_page.get_to() == ADDRESS_TO, f"Expected {ADDRESS_TO}, but got {routes_page.get_to()}"
+# Checks if Routes is up and running. Do not change
+def is_url_reachable(url):
+    """Check if the URL can be reached. Pass the URL for Urban Routes as a parameter.
+    If it can be reached, it returns True, otherwise it returns False"""
 
-    def test_select_plan(self):
-        self.driver.get(URBAN_ROUTES_URL)  # Open the URL for this test
-        routes_page = UrbanRoutesPage(self.driver)
-        routes_page.select_supportive_plan()  # Select the 'Supportive' plan
-        assert routes_page.get_selected_tariff() == "Supportive", f"Expected 'Supportive' plan, but got {routes_page.get_selected_tariff()}"
+    import ssl
+    import urllib.request
 
-    def test_fill_phone_number(self):
-        self.driver.get(URBAN_ROUTES_URL)  # Open the URL for this test
-        routes_page = UrbanRoutesPage(self.driver)
-        routes_page.fill_phone_number(PHONE_NUMBER)  # Fill the phone number
-        assert routes_page.get_phone_number() == PHONE_NUMBER, f"Expected {PHONE_NUMBER}, but got {routes_page.get_phone_number()}"
+    try:
+        ssl_ctx = ssl.create_default_context()
+        ssl_ctx.check_hostname = False
+        ssl_ctx.verify_mode = ssl.CERT_NONE
 
-    def test_add_credit_card(self):
-        self.driver.get(URBAN_ROUTES_URL)  # Open the URL for this test
-        routes_page = UrbanRoutesPage(self.driver)
-        routes_page.add_credit_card(CARD_NUMBER, CARD_EXPIRY_DATE, CARD_CVV)  # Add the credit card details
-        assert routes_page.get_card_number() == CARD_NUMBER, f"Expected card number {CARD_NUMBER}, but got {routes_page.get_card_number()}"
+        with urllib.request.urlopen(url, context=ssl_ctx) as response:
+            # print("Response Status Code:", response.status) #for debugging purposes
+            if response.status == 200:
+                 return True
+            else:
+                return False
+    except Exception as e:
+        print (e)
 
-    def test_write_driver_comment(self):
-        self.driver.get(URBAN_ROUTES_URL)  # Open the URL for this test
-        routes_page = UrbanRoutesPage(self.driver)
-        routes_page.write_driver_comment(MESSAGE_FOR_DRIVER)  # Write a comment for the driver
-        assert routes_page.get_comment() == MESSAGE_FOR_DRIVER, f"Expected comment '{MESSAGE_FOR_DRIVER}', but got {routes_page.get_comment()}"
-
-    @classmethod
-    def teardown_class(cls):
-        logging.info("Closing the browser.")
-        cls.driver.quit()
+    return False
